f9a7ab5852023b89f436f71993c91f76bb768db2
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-03-14 22:59:14 +0100

    mělo by fungovat, ale nefunguje

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const dice = document.querySelector(".dice")
 
 const changeSide = () => {
     side = side + 1
+    if (side === 7) {
+        side = 1
+    }
     dice.src = `img/side${side}.svg`
 }
 
