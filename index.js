a0e7f6f3502d6ef436ae646801b9cbf3eae5b129
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-03-14 22:35:22 +0100

    zkouška

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('funguju!');
+const side = 1
+
+const changeSide = document.querySelector(img)
+
+
+changeSide.textContent(`<img class="dice" src="img/side${
+    changeSide.addEventListener("keydown", () => {
+        return side + 1
+    })
+}.svg" /`>)
