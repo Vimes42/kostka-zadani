dc0270b9657e95721c3d332e664c09705c7619df
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-03-14 22:43:14 +0100

    zkouška

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 console.log('funguju!');
 const side = 1
 
-const changeSide = document.querySelector(img)
+const dice = document.querySelector(img)
 
+const changeSide = () => {
+    side = side + 1
+    dice.src = `img/side${side}.svg`
+}
 
-changeSide.textContent(`<img class="dice" src="img/side${
-    changeSide.addEventListener("keydown", () => {
-        return side + 1
-    })
-}.svg" /`>)
+dice.addEventListener("keydown", changeSite)
